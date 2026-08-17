5cce894b70eafd93a58b5e1d3c2bddee4c126e83
domepfa <onboard89@gmail.com>
2026-08-17 06:24:18 +0200

    Create sw-fixseil.js

---
+++ sw-fixseil.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'fixseil-shell-v1';
+const SHELL_ASSETS = [
+  './fixseil.html', './manifest-fixseil.json', './app-icon.jpg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== 'GET' || url.origin !== location.origin) return;
+
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const fetchPromise = fetch(e.request)
+        .then((networkResponse) => {
+          caches.open(CACHE_NAME).then((cache) => cache.put(e.request, networkResponse.clone()));
+          return networkResponse;
+        })
+        .catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
