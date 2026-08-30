9b76a45118faa4f7da556fc08513a8a25b5f0cd3
domepfa <onboard89@gmail.com>
2026-08-30 08:46:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bergtouren-shell-v6';
+const CACHE_NAME = 'bergtouren-shell-v7';
 const SHELL_ASSETS = [
   './', './index.html', './fixseil.html', './0-shared.js',
   './manifest.json', './manifest-fixseil.json',
